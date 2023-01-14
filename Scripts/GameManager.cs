e8fb6a44a27f5bbdd510f20d8492790d0c8ec267
Accelerator87 <n126970@gmail.com>
2023-01-15 04:03:00 +0800

    Add compass scripts

--- Scripts/GameManager.cs
+++ Scripts/GameManager.cs
@@ -9,7 +9,7 @@ public class GameManager : MonoBehaviour
     [SerializeField] private Transform[] positions;
     [SerializeField] private Transform[] players;
     [SerializeField] private Transform treasure, exit;
-    void Start()
+    private void Start()
     {
         // randomly shuffle an array positions
         int PosLen = positions.Length;
@@ -33,5 +33,58 @@ public class GameManager : MonoBehaviour
 
         // randomly set the position of treasure
         exit.position = children[children.Length - 1].position;
+    }
+
+    private float TimePassed = 0;
+    private bool DoOnetime = true;
+    [SerializeField] private Transform[] TreasureCompass, ExitCompass;
+    private void Update() {
+        if(DoOnetime) {
+            GenCompass();
+            TimePassed += Time.deltaTime;
+        }
+        if(TimePassed > 2f) {
+            DoOnetime = false;
+            TimePassed = 0;
+            DesCompass();
+        }
+    }
+
+    // generate campass
+    private void GenCompass() {
+        for(int i = 0; i < players.Length; i ++) {
+            float x = treasure.position.x - players[i].position.x;
+            float y = treasure.position.y - players[i].position.y;
+            string name = "Compass";
+            if(x > 0 && y > 0) name += 1.ToString();
+            if(x < 0 && y > 0) name += 2.ToString();
+            if(x < 0 && y < 0) name += 3.ToString();
+            if(x > 0 && y < 0) name += 4.ToString();
+            for(int j = 0; j < TreasureCompass[i].childCount; j ++) {
+                if(TreasureCompass[i].GetChild(j).name == name) {
+                    TreasureCompass[i].GetChild(j).gameObject.SetActive(true);
+                }
+            }
+
+            x = exit.position.x - players[i].position.x;
+            y = exit.position.y - players[i].position.y;
+            name = "Compass";
+            if(x > 0 && y > 0) name += 1.ToString();
+            if(x < 0 && y > 0) name += 2.ToString();
+            if(x < 0 && y < 0) name += 3.ToString();
+            if(x > 0 && y < 0) name += 4.ToString();
+            for(int j = 0; j < ExitCompass[i].childCount; j ++) {
+                if(ExitCompass[i].GetChild(j).name == name) {
+                    ExitCompass[i].GetChild(j).gameObject.SetActive(true);
+                }
+            }
+        }
+    }
+
+    private void DesCompass() {
+        for(int i = 0; i < players.Length; i ++) {
+            Destroy(TreasureCompass[i].gameObject);
+            Destroy(ExitCompass[i].gameObject);
+        }
     }
 }
